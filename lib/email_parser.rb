0c5e0edd1811f001cf40f7f7e5757b4de5d78cd0
Nina Angelo <ninacelie@gmail.com>
2019-12-21 22:05:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,4 +4,13 @@ class EmailAddressParser
   def initialize(csv_emails)
     @csv_emails = csv_emails
   end
+
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
+
 end
